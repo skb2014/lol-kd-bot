bd61396b78d5f8a786c3b89efb6e621612a16f08
LittleYay-54 <suvan.sunkavalli@gmail.com>
2025-12-01 00:39:03 -0500

    changed main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import os
-from dotenv import load_dotenv
 from discord_functionality import *
 import logging
+import os
+from dotenv import load_dotenv
 
-load_dotenv()  # Load the .env file to environment variables
-discord_token = os.getenv('DISCORD_TOKEN')  # Get the token from the environment variables
+load_dotenv()
+discord_token = os.getenv('DISCORD_TOKEN')
 
-# just to see any errors and such
-handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+def main():
+    # just to see any errors and such
+    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
-# bot is defined in discord_functionality.py
-bot.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
+    # bot is defined in discord_functionality.py
+    bot.run(discord_token, log_handler=handler, log_level=logging.DEBUG)
+
+if __name__ == "__main__":
+    main()
