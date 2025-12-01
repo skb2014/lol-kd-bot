28f44541dfe230e843a36bb0365ea401eb986d28
LittleYay-54 <suvan.sunkavalli@gmail.com>
2025-12-01 00:35:54 -0500

    added some documentation, slightly optimized the code, moved riot and discord info into the .env file

--- discord_functionality.py
+++ discord_functionality.py
@@ -2,62 +2,65 @@ import discord
 from discord.ext import commands, tasks
 from riot_functionality import *
 import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+guild_ids = list(map(int, os.getenv('DISCORD_GUILD_IDS').split(',')))
+channel_ids = list(map(int, os.getenv('DISCORD_CHANNEL_IDS').split(',')))
+guilds = []
+channels = []
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.guilds = True
-
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-guilds = []
-channels = []
-testing_guild_id = 1288788236608737281
-bchat_guild_id = 837511484589735936
-bottesting_channel_id = 1443525957271945267
-botshit_channel_id = 854026996560560208
+async def print_match_kda(riot_id, kda):
+    """Prints the Riot ID and KDA in each of the given discord channels."""
+    for channel in channels:
+        await channel.send(f'{riot_id} has just played a game! KDA: {kda["kills"]}/{kda["deaths"]}/{kda["assists"]}')
 
-@tasks.loop(seconds=5)
-async def infinite_loop():
-    (print('hello'))
+@tasks.loop(seconds=15)
+async def update_matches_loop():
+    """Repeatedly checks all players for new matches, and if one is found, the bot types their KDA in the given discord channels.
+    The most recent match IDs are saved in a .json file."""
+    print("Checking for new matches...")
+    # read .json file
     with open('most_recent_matches.json', 'r') as f:
         most_recent_matches = json.load(f)
-    temp_dict = {}
-    for riot_id in riot_IDs:
-        match_id = get_most_recent_match(get_puuid_from_riot_id(riot_id))
-        temp_dict[riot_id] = match_id
+    most_recent_matches_updated = {}
+    for riot_id in riot_ids:
+        puuid = get_puuid_from_riot_id(riot_id)
+        match_id = get_most_recent_match(puuid)
         if riot_id not in most_recent_matches:
-            for channel in channels:
-                print(channel)
-                kda = get_kda_from_most_recent_match(get_puuid_from_riot_id(riot_id))
-                await channel.send(f'{riot_id} has just played a game! KDA: {kda['kills']}/{kda['deaths']}/{kda['assists']}')
+            kda = get_kda_from_most_recent_match(puuid, match_id)
+            await print_match_kda(riot_id, kda)
         elif most_recent_matches[riot_id] != match_id:
-            for channel in channels:
-                print(channel)
-                kda = get_kda_from_most_recent_match(get_puuid_from_riot_id(riot_id))
-                await channel.send(f'{riot_id} has just played a game! KDA: {kda['kills']}/{kda['deaths']}/{kda['assists']}')
-    # overwrite most recent matches
+            kda = get_kda_from_most_recent_match(puuid, match_id)
+            await print_match_kda(riot_id, kda)
+        most_recent_matches_updated[riot_id] = match_id
+    # update the .json file
     with open("most_recent_matches.json", "w") as f:
-        json.dump(temp_dict, f, indent=4)
-
+        json.dump(most_recent_matches_updated, f, indent=4)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
     guilds.clear()
-    guilds.append(bot.get_guild(testing_guild_id))
-    guilds.append(bot.get_guild(bchat_guild_id))
+    for guild_id in guild_ids:
+        guild = bot.get_guild(guild_id)
+        guilds.append(guild)
+        print(f'Added guild: {guild.name}')
     channels.clear()
-    channels.append(bot.get_channel(bottesting_channel_id))
-    channels.append(bot.get_channel(botshit_channel_id))
+    for channel_id in channel_ids:
+        channel = bot.get_channel(channel_id)
+        channels.append(channel)
+        print(f'Added channel: {channel.name}')
     try:
         synced = await bot.tree.sync()  # Sync all slash commands
         print(f'Synced {len(synced)} command(s)')
     except Exception as e:
-        print(f"Failed to sync commands: {e}")
-    infinite_loop.start()
-
-@bot.tree.command(name='hello', description='Says hello!', guilds=guilds)
-async def hello_command(interaction: discord.Interaction):
-    await interaction.response.send_message('Hello There!')
-
+        print(f'Failed to sync commands: {e}')
+    update_matches_loop.start()
 
--- riot_functionality.py
+++ riot_functionality.py
@@ -1,34 +1,35 @@
 import os
 from dotenv import load_dotenv
 import requests
-import json
 
 load_dotenv()
 riot_api_key = os.getenv('RIOT_API_KEY')
 routing_region = os.getenv('ROUTING_REGION')
-
-riot_IDs = ['LittleYay#NA1', 'voidfłight#NA1', 'Mewzer2#NA1', 'UnsaltedSalt#NA1', 'skb#4559', 'ASneakySquirrel#NA1']
+riot_ids = os.getenv('RIOT_IDS').split(',')
 
 def get_puuid_from_riot_id(riot_id):
+    """Gets the unique PUUID for an account using their Riot ID which looks like GameName#TagLine"""
     game_name, tag_line = riot_id.split('#')
     url = f"https://{routing_region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?api_key={riot_api_key}"
     response = requests.get(url)
+    print(f"request sent to riot/account/v1/accounts/by-riot-id/, response code: {response.status_code}")
     return response.json()['puuid']
 
 def get_most_recent_match(puuid):
+    """Gets the match ID of the most recent match that the player with the specified PUUID played"""
     url = f"https://{routing_region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=1&api_key={riot_api_key}"
     response = requests.get(url)
+    print(f"request sent to lol/match/v5/matches/by-puuid/, response code: {response.status_code}")
     return response.json()[0]
-# "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={riot_api_key}"
 
-def get_kda_from_most_recent_match(puuid):
-    match_id = get_most_recent_match(puuid)
+def get_kda_from_most_recent_match(puuid, match_id):
+    """Gets the KDA of the player with the specified PUUID in their most recent match, returned as a dictionary with keys 'kills', 'deaths', 'assists'"""
     url = f"https://{routing_region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={riot_api_key}"
     response = requests.get(url)
+    print(f"request sent to lol/match/v5/matches/, response code: {response.status_code}")
     player_index = response.json()['metadata']['participants'].index(puuid)
     player_data = response.json()['info']['participants'][player_index]
     kills = player_data['kills']
     deaths = player_data['deaths']
     assists = player_data['assists']
     return {'kills': kills, 'deaths': deaths, 'assists': assists}
-
