58cec6c4cf8e04f5645d4450cf77ebd2f255456e
LittleYay-54 <suvan.sunkavalli@gmail.com>
2025-12-01 21:25:42 -0500

    added some error handling in case api requests fail

--- discord_functionality.py
+++ discord_functionality.py
@@ -18,10 +18,13 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 async def print_match_kda(riot_id, kda):
     """Prints the Riot ID and KDA in each of the given discord channels."""
+    if kda is None:
+        print(f"No KDA found for {riot_id}, returning")
+        return
     for channel in channels:
         await channel.send(f'{riot_id} has just played a game! KDA: {kda["kills"]}/{kda["deaths"]}/{kda["assists"]}')
 
-@tasks.loop(seconds=15)
+@tasks.loop(seconds=30)
 async def update_matches_loop():
     """Repeatedly checks all players for new matches, and if one is found, the bot types their KDA in the given discord channels.
     The most recent match IDs are saved in a .json file."""
@@ -36,13 +39,16 @@ async def update_matches_loop():
         print(f"{riot_id}'s PUUID is {puuid}")
         match_id = get_most_recent_match(puuid)
         print(f"{riot_id}'s most recent match ID is {match_id}")
-        if riot_id not in most_recent_matches:
-            kda = get_kda_from_most_recent_match(puuid, match_id)
-            await print_match_kda(riot_id, kda)
-        elif most_recent_matches[riot_id] != match_id:
-            kda = get_kda_from_most_recent_match(puuid, match_id)
-            await print_match_kda(riot_id, kda)
-        most_recent_matches_updated[riot_id] = match_id
+        if match_id is not None:
+            if riot_id not in most_recent_matches:
+                kda = get_kda_from_most_recent_match(puuid, match_id)
+                await print_match_kda(riot_id, kda)
+            elif most_recent_matches[riot_id] != match_id:
+                kda = get_kda_from_most_recent_match(puuid, match_id)
+                await print_match_kda(riot_id, kda)
+            most_recent_matches_updated[riot_id] = match_id
+        else:
+            most_recent_matches_updated[riot_id] = most_recent_matches[riot_id]
     # update the .json file
     with open("most_recent_matches.json", "w") as f:
         json.dump(most_recent_matches_updated, f, indent=4)
--- riot_functionality.py
+++ riot_functionality.py
@@ -7,29 +7,54 @@ riot_api_key = os.getenv('RIOT_API_KEY')
 routing_region = os.getenv('ROUTING_REGION')
 riot_ids = os.getenv('RIOT_IDS').split(',')
 
+def response_checker(response):
+    """Checks the status code of a response and raises an exception if it's not 200"""
+    if response.status_code != 200:
+        print(f"Request failed with status code {response.status_code}, response text: {response.text}")
+        return False
+    else:
+        return True
+
 def get_puuid_from_riot_id(riot_id):
     """Gets the unique PUUID for an account using their Riot ID which looks like GameName#TagLine"""
     game_name, tag_line = riot_id.split('#')
     url = f"https://{routing_region}.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}?api_key={riot_api_key}"
     response = requests.get(url)
-    print(f"request sent to riot/account/v1/accounts/by-riot-id/, response code: {response.status_code}")
-    return response.json()['puuid']
+    print(f"request sent to riot/account/v1/accounts/by-riot-id/")
+    if response_checker(response):
+        return response.json()['puuid']
+    else:
+        return None
 
 def get_most_recent_match(puuid):
     """Gets the match ID of the most recent match that the player with the specified PUUID played"""
+    if puuid is None:
+        print("No PUUID entered, returning None")
+        return None
     url = f"https://{routing_region}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=1&api_key={riot_api_key}"
     response = requests.get(url)
     print(f"request sent to lol/match/v5/matches/by-puuid/, response code: {response.status_code}")
-    return response.json()[0]
+    if response_checker(response):
+        return response.json()[0]
+    else:
+        return None
 
 def get_kda_from_most_recent_match(puuid, match_id):
     """Gets the KDA of the player with the specified PUUID in their most recent match, returned as a dictionary with keys 'kills', 'deaths', 'assists'"""
+    if puuid is None:
+        print("No PUUID entered, returning None")
+        return None
+    if match_id is None:
+        print("No match ID entered, returning None")
     url = f"https://{routing_region}.api.riotgames.com/lol/match/v5/matches/{match_id}?api_key={riot_api_key}"
     response = requests.get(url)
     print(f"request sent to lol/match/v5/matches/, response code: {response.status_code}")
-    player_index = response.json()['metadata']['participants'].index(puuid)
-    player_data = response.json()['info']['participants'][player_index]
-    kills = player_data['kills']
-    deaths = player_data['deaths']
-    assists = player_data['assists']
-    return {'kills': kills, 'deaths': deaths, 'assists': assists}
+    if response_checker(response):
+        player_index = response.json()['metadata']['participants'].index(puuid)
+        player_data = response.json()['info']['participants'][player_index]
+        kills = player_data['kills']
+        deaths = player_data['deaths']
+        assists = player_data['assists']
+        return {'kills': kills, 'deaths': deaths, 'assists': assists}
+    else:
+        return None
